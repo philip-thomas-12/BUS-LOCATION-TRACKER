7eec65c44fc0c76355110180ecc754d4d5e38fd8
PHILIP THOMAS <thomasphil6889@gmail.com>
2026-06-11 23:59:12 +0530

    Add Bus model to tracker app

--- tracker/apps.py
+++ tracker/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class TrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tracker'
+
+
+class Bus(models.Model):
+    name = models.CharField(max_length=100)
+    route = models.CharField(max_length=100)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.route})"
